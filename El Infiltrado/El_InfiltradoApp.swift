fdeb7092b5cea8dc0be60e286972a7ac1cf33018
Eugenio Barquín <eu.barquin@me.com>
2025-11-25 22:18:25 +0100

    Initial commit

---
+++ El Infiltrado/El_InfiltradoApp.swift
@@ -0,0 +1,17 @@
+//
+//  El_InfiltradoApp.swift
+//  El Infiltrado
+//
+//  Created by Eugenio Barquin on 25/11/25.
+//
+
+import SwiftUI
+
+@main
+struct El_InfiltradoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ El InfiltradoUITests/El_InfiltradoUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  El_InfiltradoUITestsLaunchTests.swift
+//  El InfiltradoUITests
+//
+//  Created by Eugenio Barquin on 25/11/25.
+//
+
+import XCTest
+
+final class El_InfiltradoUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
